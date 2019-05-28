e6d7e83c98568a9011bf8ab032665520d9bb837f
Brigitta Rucz <me@example.com>
2019-05-28 16:30:35 +0200

    mobile view - modal and shops

---
+++ javascript-modal.js
@@ -0,0 +1,9 @@
+function toggleModal() {
+    let productModal = document.querySelector(".modal");
+    productModal.style.display = "none";
+}
+
+function openModal() {
+    let productModal = document.querySelector(".modal");
+    productModal.style.display = "block";
+}
