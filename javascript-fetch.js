a74ae7b487e93fee3f664277882ca45f65a2ecf5
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-06-01 03:03:37 +0200

    loading listings with js

---
+++ javascript-fetch.js
@@ -0,0 +1,51 @@
+let baseLink = "http://inuk.one/stender/wp-json/wp/v2/";
+const linkParameters = new URLSearchParams(window.location.search);
+const categID = linkParameters.get("cat");
+
+const template = document.querySelector("template").content;
+const parentElement = document.querySelector(".container-item")
+
+
+
+function loadListings(link) {
+    fetch(link + "product?categories=" + categID + "&per_page=40&_embed").then(e => e.json()).then(data => loadApi(data));
+}
+
+
+function loadCategories(link) {
+    fetch(link + "categories").then(e => e.json()).then(data => findCategory(data));
+}
+
+
+
+
+function loadApi(data) {
+    data.forEach(listing => {
+        //console.log(listing);
+        const clone = template.cloneNode(true);
+        clone.querySelector("h4").textContent = listing.title.rendered;
+        clone.querySelector(".image-item").src = listing._embedded["wp:featuredmedia"][0].source_url;
+        clone.querySelector(".image-item").alt = listing.featured_image_alt;
+        clone.querySelector(".price").textContent = listing.price;
+        clone.querySelector(".material").textContent = listing.material;
+        clone.querySelector(".collection").textContent = listing.collection;
+
+        parentElement.appendChild(clone);
+    })
+};
+
+
+
+function findCategory(data) {
+    data.forEach(category => {
+        console.log(category);
+        if (categID == category.id) {
+            document.querySelector("h1").textContent = category.name;
+        }
+    })
+};
+
+
+
+loadCategories(baseLink);
+loadListings(baseLink);
--- script.js
+++ script.js
@@ -6,15 +6,6 @@ function closenav() {
     document.getElementById("navbar").style.width = "0%";
 }
 
-//function fix(){
-//document.getElementById("header").style.height ="100%";}
-//
-//document.getElementsById("closebtn").addEventListener("click", fixClose);
-//
-//function fixClose(){
-//document.getElementById("header").style.height ="0%";}
-
-
-
-
-
+function openCategPage(id){
+   window.location.href = "shop-category.html?cat=" + id;
+}
